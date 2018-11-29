10176acbfcc9867c0054723f9b8d97144cabf8f1
DanielAntonio <DanielAntonio@DANIEL-LAP>
2018-11-28 19:47:50 -0500

    Agregar .gitignore y .gitattributes.

---
+++ ToDoList/MainActivity.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using ToDoList.Helper;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login.Widget;
+
+namespace ToDoList
+{
+    [Activity(Label = "ToDoList", Theme = "@style/Theme.AppCompat.Light", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IFacebookCallback
+    {
+        private ICallbackManager mFBCallManager;
+        private MyProfileTracker mprofileTracker;
+        private TextView TxtFirstName;
+        private TextView TxtLastName;
+        private TextView TxtName;
+        private ProfilePictureView mprofile;
+        LoginButton BtnFBLogin;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            FacebookSdk.SdkInitialize(this.ApplicationContext);
+            mprofileTracker = new MyProfileTracker();
+            mprofileTracker.mOnProfileChanged += mProfileTracker_mOnProfileChanged;
+            mprofileTracker.StartTracking();
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.login);
+            BtnFBLogin = FindViewById<LoginButton>(Resource.Id.fblogin);
+            TxtFirstName = FindViewById<TextView>(Resource.Id.TxtFirstname);
+            TxtLastName = FindViewById<TextView>(Resource.Id.TxtLastName);
+            TxtName = FindViewById<TextView>(Resource.Id.TxtName);
+            mprofile = FindViewById<ProfilePictureView>(Resource.Id.ImgPro);
+            BtnFBLogin.SetReadPermissions(new List<string> {
+        "user_friends",
+        "public_profile"
+    });
+            mFBCallManager = CallbackManagerFactory.Create();
+            BtnFBLogin.RegisterCallback(mFBCallManager, this);
+        }
+
+        internal void LoadTaskList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCancel() { }
+        public void OnError(FacebookException p0) { }
+        public void OnSuccess(Java.Lang.Object p0) { }
+        void mProfileTracker_mOnProfileChanged(object sender, OnProfileChangedEventArgs e)
+        {
+            if (e.mProfile != null)
+            {
+                try
+                {
+                    TxtFirstName.Text = e.mProfile.FirstName;
+                    TxtLastName.Text = e.mProfile.LastName;
+                    TxtName.Text = e.mProfile.Name;
+                    mprofile.ProfileId = e.mProfile.Id;
+                }
+                catch (Java.Lang.Exception ex) { }
+            }
+            else
+            {
+                TxtFirstName.Text = "First Name";
+                TxtLastName.Text = "Last Name";
+                TxtName.Text = "Name";
+                mprofile.ProfileId = null;
+            }
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            mFBCallManager.OnActivityResult(requestCode, (int)resultCode, data);
+        }
+        public class MyProfileTracker : ProfileTracker
+        {
+            public event EventHandler<OnProfileChangedEventArgs> mOnProfileChanged;
+            protected override void OnCurrentProfileChanged(Profile oldProfile, Profile newProfile)
+            {
+                if (mOnProfileChanged != null)
+                {
+                    mOnProfileChanged.Invoke(this, new OnProfileChangedEventArgs(newProfile));
+                }
+            }
+        }
+        public class OnProfileChangedEventArgs : EventArgs
+        {
+            public Profile mProfile;
+            public OnProfileChangedEventArgs(Profile profile)
+            {
+                mProfile = profile;
+            }
+        }
+
+
+    }
+}
+
---
+++ ToDoList/tasks.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using ToDoList.Helper;
+namespace ToDoList
+{
+    class tasks: AppCompatActivity
+    {
+        EditText edtTask;
+        DbHelper dbHelper;
+        CustomAdapter adapter;
+        ListView lstTask;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_add:
+                    edtTask = new EditText(this);
+                    Android.Support.V7.App.AlertDialog alertDialog =
+                        new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("Add new task")
+                        .SetMessage("What is your next task?")
+                        .SetView(edtTask)
+                        .SetPositiveButton("Add", OkAction)
+                        .SetNegativeButton("Cancel", CancelAction)
+                        .Create();
+                    alertDialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+
+        }
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = edtTask.Text;
+            dbHelper.insertNewTask(task);
+            LoadTaskList();
+        }
+
+        public void LoadTaskList()
+        {
+            List<string> taskList = dbHelper.getTaskList();
+            adapter = new CustomAdapter(this, taskList, dbHelper);
+            lstTask.Adapter = adapter;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+            dbHelper = new DbHelper(this);
+            lstTask = FindViewById<ListView>(Resource.Id.lstTask);
+
+            LoadTaskList();
+
+        }
+
+
+    }
+}
